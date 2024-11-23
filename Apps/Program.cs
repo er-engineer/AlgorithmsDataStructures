6f1a90eb7af573abe75c6e645fe79d186e17def8
Eren Memiş <69437351+er-engineer@users.noreply.github.com>
2024-11-23 21:45:49 +0300

    Add project files.

---
+++ Apps/Program.cs
@@ -0,0 +1,16 @@
+﻿using DataStructures.Arrays;
+
+namespace Apps
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var array = new Array<int>(15,22,35,45,55,32,48);
+
+            foreach (var item in array)
+                Console.WriteLine(item);
+            
+        }
+    }
+}
---
+++ DataStructures/Arrays/Array.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures.Arrays
+{
+    public class Array<T> : IEnumerable<T>, ICloneable
+    {
+        public int Count { get; private set; }
+        private T[] InnerList;
+        public int Capacity => InnerList.Length;
+
+        public Array()
+        {
+            InnerList = new T[0];
+            Count = 0;
+        }
+        public Array(params T[] initial)
+        {
+            InnerList = new T[initial.Length];
+            Count = 0;
+            foreach (var item in initial)
+            {
+                Add(item);
+            }
+        }
+
+        public Array(IEnumerable<T> collection)
+        {
+            InnerList = new T[collection.ToArray().Length];
+            Count = 0;
+            foreach (var item in collection)
+                Add(item);
+        }
+
+        public void Add(T item)
+        {
+            
+            T[] newArray = new T[Count + 1];
+            for(int i = 0; i < Count; i++)
+            {
+                newArray[i] = InnerList[i];
+            }
+            InnerList = newArray;
+            InnerList[Count++] = item;
+        }
+
+        public T GetElementAt(int index)
+        { 
+            return InnerList[index];
+        }
+
+        public object Clone()
+        {
+            return this.MemberwiseClone();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return InnerList.Select(x => x).GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
